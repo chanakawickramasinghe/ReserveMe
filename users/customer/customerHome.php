a0ad71b95b9af9e10bb73b50b181ebfd64adb178
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2020-10-08 22:42:09 +0530

    Thursday 2020/10/08

---
+++ users/customer/customerHome.php
@@ -0,0 +1,36 @@
+<html>
+    <head> 
+        <title>ReserveMe</title>
+        <link rel="stylesheet" href="../../CSS/nav.css">
+        <link rel="stylesheet" href="../../CSS/search.css">
+    </head>
+    <body>
+        <nav>
+            <label class="logo"><a href="www.fb.com"><img src="../../images/test.jpg" height=800% ></a></label>
+            <ul>
+                <li><a>Table Reservations</a></li>
+                <li><a>Hall Reservations</a></li>
+                <li><a href="../../index.php">Logout</a></li>
+                <!-- <li><a href="register.php">Register</a></li>    -->
+            </ul>
+        </nav>
+        <br/>
+        <div class="header">
+            <form>
+                <div class="form-box">
+                    <input type="text" class="search-feild restaurant" placeholder="Search for Restaurant...">
+                    <select name="location" class="search-feild location">
+                        <option value="colombo">Colombo</option>
+                        <option value="kandy">Kandy</option>
+                        <option value="gampaha">Gampaha</option>
+                    </select>
+                    <select name="search-type" class="search-feild search-type">
+                        <option value="restaurant">Restaurant</option>
+                        <option value="dish">Dish</option>
+                    </select>
+                    <button name="search" class="search-btn" type="button">Search</button>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>

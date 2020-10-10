9e114f312a4ba7bfd91bebe4123710de0ccf2517
Aamir-97 <msaamirali123@gmail.com>
2020-10-10 19:39:13 +0530

    User registration and login

---
+++ users/customer/customerReg.php
@@ -0,0 +1,61 @@
+<html>  
+<head>
+    <title>Customer Registration</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">   
+
+
+
+</head>
+    
+<body class="register-body">
+
+      
+        <div class="row-100">
+            <div class="login-box">
+              
+                <h1>Register Here</h1>
+                
+              
+
+            <h2 class="error-msg"><?php include_once('../../includes/message.php'); ?>
+
+                <form action="register-submit.php" method="post">                   
+                    <p>Name</p>
+                    <input type="text" name="name" placeholder="Your name" required>    
+
+                    <p>Email address</p>
+                    <input type="email" name="email" placeholder="your email" required >
+
+                    <p>Password</p>
+                    <input type="password" name="password" placeholder="Enter Password" id="password" onkeyup='check()' required>
+
+                    <p>Confirm Password</p>
+                    <input type="password" name="password2" placeholder="Re-Enter Password" id="confirm_password" onkeyup='check()' required>
+                    <span id='message' ></span>
+
+                    <p>Contact number</p>
+                    <input type="text" name="contact" placeholder="Mobile number">            
+
+                    <input type="submit" name="submit" value="Register" id="regsubmit" onclick="alertbox()" required>
+                </form>       
+            </div>
+        </div>
+    
+    
+        <script>
+        var check = function() {
+            if (document.getElementById('password').value == document.getElementById('confirm_password').value){
+                    document.getElementById('message').style.color = 'green';
+                    document.getElementById('message').innerHTML = 'Password is matching';
+            } 
+            else{
+                document.getElementById('message').style.color = 'red';
+                document.getElementById('message').style.backgroundColor = 'white';
+                document.getElementById('message').innerHTML = 'Password does not match';
+            }
+        }
+    
+        </script>
+</body>
+
+</html>

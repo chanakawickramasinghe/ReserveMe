501885de0b3ca185b9df3d00da58039ad5fdf952
Aamir-97 <msaamirali123@gmail.com>
2020-10-12 11:48:05 +0530

    for new get

--- includes/login.php
+++ includes/login.php
@@ -14,7 +14,7 @@
     
         <div class="login-box" style="width:350px">
 
-        <h3 class="error-msg"><?php include('message.php'); ?></h3>
+        <h3 class="logged-in-msg"><?php include('message.php'); ?></h3>
         
         <img src="../images/profile.png" class="avatar">
         <br>

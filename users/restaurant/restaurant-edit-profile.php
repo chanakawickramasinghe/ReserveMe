2ed751f06fa8d951456b9622cf2d811e23fb5806
Nuwan Fernando <54792829+lifeofhassa@users.noreply.github.com>
2020-10-14 17:20:28 +0530

    Add files via upload

--- users/restaurant/restaurant-edit-profile.php
+++ users/restaurant/restaurant-edit-profile.php
@@ -35,18 +35,40 @@
         $userquery = mysqli_query($connection,$sql);
             while($row = mysqli_fetch_assoc($userquery)){
 
-                echo "<tr>
-                        <th> Res ID </th><td>".$row['res_id']." </td>
-                    </tr>
-                    <tr>                  
-                        <th> Name </th><td>".$row['res_name']." </td>
-                    </tr>
-                    <tr>
-                        <th> Email </th><td>".$row['res_add']." </td>
-                    </tr>
-                    <tr>
-                        <th> Contact No </th><td>".$row['res_tel']."</td>
-                    </tr>";
+                </br>
+                <tr>
+                <th>Res ID</th><td>".$row['res_id']."</td>
+                </tr>
+                <tr>                  
+                <th>Name</th><td>".$row['res_name']."</td>
+                </tr>
+                <tr>
+                <th>Email</th><td>".$row['res_email']."</td>
+                </tr>
+                <tr>
+                <th>ContactNo</th><td>".$row['res_tel']."</td>
+                </tr>
+                <tr>
+                <th>Address Line 01</th><td>".$row['res_add_line1']."</td>
+                </tr>
+                <tr>
+                <th>Address Line 02</th><td>".$row['res_add_line2']."</td>
+                </tr>
+                <tr>
+                <th>City</th><td>".$row['res_add_line3']."</td>
+                </tr>
+                <tr>
+                <th>Location</th><td>".$row['res_location']."</td>
+                </tr>
+                <tr>
+                <th>Floor Plan</th><td>".$row['res_floor']."</td>
+                </tr>
+                <tr>
+                <th>Menu</th><td>".$row['res_menu']."</td>
+                </tr>
+                <tr>
+                <th>Preorder Availability</th><td>".$row['preorder_available']."</td>
+                </tr>;
             } 
 
         //    $sql = "SELECT * FROM restaurant_category WHERE res_id='$res_id'";

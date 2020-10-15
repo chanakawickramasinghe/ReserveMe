21dfb367849f7ef048350c6b635a9049f545d1af
Chathuranga Priyadarshani <chathupriya@gmail.com>
2020-10-15 12:46:04 +0530

    improved 10/15

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <!---Start of footer-->
 <footer class="footer-distributed">
     <div class="footer-left">
-        <img src="images/test.jpg">
+        <img src="images/logo.png">
 		<h3>Reserve<span>Me</span></h3>
         <p class="footer-links">
 			<a href="#">Home</a>

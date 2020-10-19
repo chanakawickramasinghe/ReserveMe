85485628b6cbffec7c0e85d3b40a73b8a72527fa
Nuwan Fernando <54792829+lifeofhassa@users.noreply.github.com>
2020-10-19 22:27:58 +0530

    Add files via upload

--- users/restaurant/restaurant-home.php
+++ users/restaurant/restaurant-home.php
@@ -9,14 +9,13 @@
 	<link rel="stylesheet" href="../../CSS/nav.css">
     <link rel="stylesheet" href="../../CSS/search.css">
     <link rel="stylesheet" href="../../CSS/footer.css">
-    <link href="../../images/logo.png" rel="shortcut icon"/> 
 </head>
 
 <body>
     <!--Start of nav-->
     <div class="topnav" id="myTopnav">
         <a href="../../index.php">Logout</a>
-        <a href="restaurant-profile.php">Profile</a>
+        <a href="customer-profile.php">My Profile</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
@@ -52,7 +51,7 @@
                 <div class="form-box"><c>
                     <a href="#" class="search-button pulsate">Pre - Order Menu</a>
                     <a href="#" class="search-button pulsate">Update the Menu</a>
-                    <a href="../employee/employee-add.php" class="search-button pulsate">Manage Employee</a>
+                    <a href="#" class="search-button pulsate">Add Employee</a>
                     <a href="#" class="search-button pulsate">Reception Hall</a>
                 </c></div>
             </form>            
@@ -64,10 +63,4 @@
         </div>
     </body>
 </html>
-     <!--Include footer.php-->
-   <div><?php include "../../includes/footer.php" ?></div>
     
-    <!--script for onClickNav() for the navigation menu-->
-    <script src="../../js/onClickNav.js"></script>
-</body>
-</html>

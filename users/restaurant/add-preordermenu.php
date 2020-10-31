3daac6f27c3dd596a2e499fd8e6c0eee9eaaf907
Nuwan Fernando <54792829+lifeofhassa@users.noreply.github.com>
2020-10-31 15:41:27 +0530

    Add files via upload

--- users/restaurant/add-preordermenu.php
+++ users/restaurant/add-preordermenu.php
@@ -26,26 +26,23 @@
 <!--End of nav-->
 <br><br><br><br><br>
     <div class="tab" >
-            <div class="login-box" style="height:450px;padding:10px 30px">
-
-                <form name="formRestSignup" action="res_reg_submit.php" method="post" enctype="multipart/form-data">
+        <div class="login-box" style="height:450px;padding:10px 30px">
+            <form name="formRestSignup" action="add-preordermenu.php" method="post" enctype="multipart/form-data">
                 <h2 class="error-msg" style="font-size:14px"><?php include_once('../../includes/message.php'); ?></h2>
                 <br>
                 <h1 align="center">Pre - Order Menu</h1>
                 <h4 align="center">Details regarding the Menu available to place Pre-Orders</h4>
-                    <br>
-                        <p><h3>Item Name<h3></p>
-                            <input type="text" class="type-feild"   name="item_name" placeholder="Item Name" required>
-            
-                        <p><h3>Item Price<h3></p>
-                            <input type="text"  class="type-feild" name="item_price" placeholder="Price in LKR" required>
-                    <br>
-
-                        <input type="submit" name="submit" style="margin-left:220px;margin-top:-25px" class="search-btn hover" value="Submit" required>
-                        <br><br><br>
-                        <input type="submit" name="submit" style="margin-left:220px;margin-top:-25px" class="search-btn hover" value="Complete" required>
-                </form>
-            </div>
+                <br>
+                <p><h3>Item Name<h3></p>
+                <input type="text" class="type-feild"   name="item_name" placeholder="Item Name" required>
+                <p><h3>Item Price<h3></p>
+                <input type="text"  class="type-feild" name="item_price" placeholder="Price in LKR" required>
+                <br>
+                <input type="submit" name="submit" style="margin-left:220px;margin-top:-25px" class="search-btn hover" value="Submit" required>
+                <br><br><br>
+                <input type="submit" name="submit" style="margin-left:220px;margin-top:-25px" class="search-btn hover" value="Complete" required>
+            </form>
+        </div>
     <div>
 </body>
 

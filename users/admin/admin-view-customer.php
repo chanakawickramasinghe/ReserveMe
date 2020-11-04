f98a6f2e0fc7e817d9d4fd864a10323ba7f4e6ab
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2020-11-05 00:42:04 +0530

    admin-customer-view

--- users/admin/admin-view-customer.php
+++ users/admin/admin-view-customer.php
@@ -1,4 +1,5 @@
 <?php include('../../includes/session.php') ?>
+<?php include('../../includes/connection.php') ?>
 
 <html>
     <head>
@@ -8,6 +9,7 @@
 	    <link rel="stylesheet" href="../../CSS/nav.css">
         <link href="../../images/logo.png" rel="shortcut icon"/>
         <link rel="stylesheet" href="../../CSS/admin-dashboard.css"/>
+        <script src="../../js/manage-user-search.js"></script>
     </head>
     <body>
 
@@ -25,13 +27,63 @@
 		        <h3>Search From Email or ID</h3>
 		        <!--Input-------->
 		        <div class="search-input">
-			        <input placeholder="Enter Email or ID"/>
-			        <button class="search-btn" name="search-user" type="submit">Search</button>
+                    <input type="text" id="myInput" onkeyup="myFunctionCustomer()" 
+                    placeholder="Enter Name , Email or ID"/>
                 </div>
+                <br/>
+
             </form>
         </div>
-
+        <br/>
         
+        
+        <div>
+        
+        <!-- Adding the table with customer details -->
+        
+        <table class="user" id="myTable" border="1">
+            <tr>
+                <th>Customer ID</th>
+                <th>Customer Name</th>
+                <th>Customer Email</th>
+                <th>Contact No.</th>
+                <th>DoB</th>
+                <th>Postbox</th>
+                <th>Street</th>
+                <th>City</th>
+                <th>Ranking</th>
+                <th>Status</th>
+            </tr>
+        
+        <?php
+        
+        $customers_sql = "SELECT * FROM customer";
+        
+        $userquery = mysqli_query($connection,$customers_sql);
+        while($row = mysqli_fetch_assoc($userquery)){
+        
+            echo "
+                <tr>                  
+                    <td>".$row['user_id']."</td>
+                    <td>".$row['user_name']."</td>
+                    <td><a href=\"mailto:".$row['email']."\">".$row['email']."</a></td>
+                    <td>".$row['contact_no']."</td>
+                    <td>".$row['dob']."</td>
+                    <td>".$row['postal_number']."</td>
+                    <td>".$row['street']."</td>
+                    <td>".$row['city']."</td>
+                    <td>".$row['rankings']."</td>
+                    <td>".$row['active_status']."</td>
+                </tr>";
+                
+        } 
+        echo "</table>";
+        ?>
+        
+        
+        </div>
+
+       
 
     </body>
 </html>

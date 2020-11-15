56280da628eff603470a3f60bfe35ee99f54e614
Nuwan Fernando <54792829+lifeofhassa@users.noreply.github.com>
2020-11-15 15:31:05 +0530

    Add files via upload

--- users/employee/employee-add.php
+++ users/employee/employee-add.php
@@ -1,137 +1,75 @@
-
 <html>  
 <head>
-    <title>Add Employee</title>
-    <link rel="stylesheet" type="text/css" href="../../CSS/add-employee.css">   
+    <title>Employee Registration</title> 
+    <link href="../../images/logo.png" rel="shortcut icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/20026fc328.js" crossorigin="anonymous"></script>
-	    <link rel="stylesheet" href="../../CSS/nav.css">
-        <link rel="stylesheet" href="../../CSS/search.css">
-        <link rel="stylesheet" href="../../CSS/footer.css">
-        <link href="../../images/logo.png" rel="shortcut icon"/> 
+    <!-- Link the style sheets -->
+    <link href="../../CSS/login.css" rel="stylesheet" type="text/css" > 
+    <link href="../../CSS/nav.css" rel="stylesheet" type="text/css">
+    <link href="../../CSS/footer.css" rel="stylesheet" type="text/css">     
+    <link href="../../CSS/res_reg.css" rel="stylesheet" type="text/css" > 
+    <script crossorigin="anonymous" src="https://kit.fontawesome.com/70a642cd7c.js"></script>
+
+    <style>
+        .signin {
+            padding:10px;
+            background-color: #e9bc5c;
+            text-align: center;
+            border-radius:5px;
+        }
+    </style>
 </head>
     
-<body >
-        <?php
-        include('../../includes/connection.php');
-        include('../../includes/session.php');
-        checkSession();
-        echo "<table border=1 class=\"user\">";
-        
-        $resID = $_SESSION["resID"]; //have to add session id
-        $sql = "SELECT * FROM employee WHERE res_id='$resID'";
-        
-        $userquery = mysqli_query($connection,$sql);
-        echo " <b><tr>                  
-                <th>Name</th><th>Email</th><th>Contact No</th>
-                </tr></b>";
-        while($row = mysqli_fetch_assoc($userquery)){
-        // echo"</br></br><h1 class=div-c>Welcome " .$row['emp_name']."</h1>";
-
-            echo "          
-                <tr>
-                <td>".$row['emp_email']."</td><td>".$row['emp_name']."</td><td>".$row['emp_mobile']."</td> <td><button type=\"button\" name=\"dlt\" class=\"delete\" >Delete </button></td> <td><button value=\"delete\" >Edit</button></td>
-                </tr>
-                ";
-        } 
-        echo "</table>";
-        ?>
-       
-        <div class="row-100">
-            <div class="login-box" style="width:400px" style="height:">
+<body class="login-body">
+  
+        <div class="login-box"> 
                
-            <img src="../../images/profile.png" style="margin-top:50px"class="avatar">
-            <br><br>
-            <h1 align="center">Employee Registration</h1>
-        
+            <img src="../../images/form_icons/regform.png" class="avatar">
+            <br>
+                <h1 align="center">Add Employee</h1><br>
+
             <h2 class="error-msg"><?php include_once('../../includes/message.php'); ?></h2>
 
-                <form action="employee-add.php" method="post">                   
-                <div>   
-                    <table style="width:500px">
-                        <td>
-                            <p><b>Name</p>
-                            <input type="text" class="type-feild" name="name" placeholder="Employee name" required>   
+                <form action="employee-add-submit.php" method="post">                   
+                  
+                <table style="width:750px;float:center;align-items:center">
+                <tr>
+                    <td>
+                    <img src="../../images/form_icons/user.png" alt="user" class="icons">
+                    <label for="name">Name</label>                   
+                    <input type="text"  name="name" class="reg-type-feild" placeholder="Your name" required>    
+                    
+                    <p>
+                    <img src="../../images/form_icons/mail.png" alt="user" class="icons">
+                    <label for="email">Email address</label>
+                    <input type="email" name="email" class="reg-type-feild" placeholder="your email" required >
+                    </p>
 
-                            <p><b>Email</p>
-                            <input type="email" class="type-feild" name="email" placeholder="Email Address" required> 
+                    <img src="../../images/form_icons/phone-call.png" alt="user" class="icons">
+                    <label for="contact">Contact number</label>
+                    <input type="tel" name="contact" class="reg-type-feild" placeholder="Mobile number" pattern='^\+?\d{0,13}' >
 
-                            <p>Contact number</p>
-                            <input type="tel" name="contact" class="type-feild" placeholder="Mobile number" pattern='^\+?\d{0,13}' required>
+                    <p>
+                    <img src="../../images/form_icons/padlock.png" alt="user" class="icons">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="reg-type-feild" placeholder="Enter Password" id="password"  required>
+                    </p>
+                    
+                    <img src="../../images/form_icons/password.png" alt="user" class="icons">
+                    <label for="password2">Confirm Password</label>
+                    <input type="password" name="password2" class="reg-type-feild" placeholder="Re-Enter Password" id="confirm_password" onkeyup='check()' required>
+                    <P id='message' ></p>
 
-                            <p>Password</p>
-                            <input type="password" name="password" class="type-feild" placeholder="Enter Password" id="password"  required>
-
-                            <p>Confirm Password</p>
-                            <input type="password" name="password2" class="type-feild" placeholder="Re-Enter Password" id="confirm_password" onkeyup='check()' required>
-                            <P id='message' ></P>
-                        </td>
-                    </table>          
-                    <br>
-                   
-                <p align="center"> <input type="submit" name="submit"  class="search-btn hover" value="Register"  onclick="alertbox()" required></p>
-                </div>
-                </form>    
-            </div>
+                    </td>
+                </tr> 
+                </table>          
+            </form>    
+            
         </div>
-        <?php
-
-
-
-    if(isset($_POST['submit'])){
-	
-    // Assign input data from form to variables
-    $name = $_POST['name'];
-    $email= $_POST['email'];
-    $contact = $_POST['contact'];
-    $password = md5($_POST['password']);
-	$password2 = md5($_POST['password2']);
     
-        //Check if email already exists in both tables
-        $selectmail= "SELECT * FROM customer WHERE email ='$email' " ;
-        $allmailquery = mysqli_query($connection, $selectmail ) ;  
-        $num = mysqli_num_rows($allmailquery);
-
-        $selectmail2="SELECT * FROM restaurant WHERE res_email='$email'";
-        $allmailquery2 = mysqli_query($connection, $selectmail2 ) ;  
-        $num2 = mysqli_num_rows($allmailquery2);
-
-        $selectmail3="SELECT * FROM  employee WHERE emp_email='$email'";
-        $allmailquery3 = mysqli_query($connection, $selectmail3 ) ;  
-        $num3 = mysqli_num_rows($allmailquery3);
-
-
-        
-        if($num > 0 || $num2 > 0 || $num3 >0){
-        $message = base64_encode(urlencode("Email already exists"));
-        header('Location:employee-add.php?msg=' . $message);
-        exit();
-        }
-        
-        //Insert to Database
-        else {
-
-            
-            $registrationQuery = "INSERT INTO employee (emp_id, res_id, emp_name, emp_email, emp_mobile, emp_password) VALUES (NULL,$resID,'$name','$email','$contact','$password')";
-            if (mysqli_query($connection,$registrationQuery) == TRUE) {
-                
-                $message = base64_encode(urlencode("Registration Successful"));
-                header('Location:employee-add.php?msg=' . $message);
-				exit();
-            } 
-            
-            else {
-                
-                $message = base64_encode(urlencode("SQL Error while Registering"));
-                header('Location:employee-add.php?msg=' . $message);
-				exit();
-            }
-        }
-    }
-mysqli_close($connection);
-?>
-    
-        <script>
+    <!--script for onClickNav() for the navigation menu-->
+    <script src="../../js/onClickNav.js"></script>
+    <script>
         var check = function() {
             if (document.getElementById('password').value == document.getElementById('confirm_password').value){
                     document.getElementById('message').style.color = 'green';
@@ -144,7 +82,6 @@ mysqli_close($connection);
             }
         }
     
-        </script>
+    </script>
 </body>
-
 </html>

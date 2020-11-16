778670f4cedf871b1a8b2536c5fbb041163f09a4
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2020-11-16 23:51:38 +0530

    res-add-emp

--- users/restaurant/emp-iframe.php
+++ users/restaurant/emp-iframe.php
@@ -61,8 +61,8 @@
         }
         else{
             echo "
-            <h1>Manage Employee</h1><br>";
-            
+            <h1>Manage Employee</h1><br>
+            <h3>You have added an Employee and you can edit details</h3>";
             
             while($row = mysqli_fetch_assoc($emp_query)){
 

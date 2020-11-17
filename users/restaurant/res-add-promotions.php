76eae3a7b64b6f298d443a82b3ef9a2eca0e1720
Nuwan Fernando <54792829+lifeofhassa@users.noreply.github.com>
2020-11-17 06:25:17 +0530

    Add files via upload

---
+++ users/restaurant/res-add-promotions.php
@@ -0,0 +1,56 @@
+<?php include('../../includes/session.php') ?>
+<?php include('../../includes/connection.php') ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ReserveMe - Res Add Promotion</title>
+	    <link rel="stylesheet" href="../../CSS/nav.css">
+        <link href="../../images/logo.png" rel="shortcut icon"/>
+        <link rel="stylesheet" href="../../CSS/res-promo.css"/>
+        <script src="../../js/manage-user-search.js"></script>
+    </head>
+    <body class="login-body">
+
+        <!--Start of nav-->
+        <div class="topnav" id="myTopnav">
+        <a href="../../index.php"><img class = "logo" src="../../images/logo.png"></a>
+            <a class="navtab" href="../../index.php">Logout</a>
+            <a class="navtab" href="restaurant-home.php">Home</a>
+            <a class="navtab" href="res-promotions.php">Promotions</a>
+        </div>
+        <!--End of nav-->
+        <br><br>
+  
+    <div class="login-box"> 
+        <center><h1 style="color:#ffbb01;">Add Promotion</h1></center>
+        <br><br>
+        <div class="centered-view">
+        <form class="add-promo-form" method="post" action="res-add-promotion-submit.php" enctype="multipart/form-data">
+            <table class="add-promo-form">
+                <tr>
+                    <th>Starting Date</th>
+                    <td><input class="input-feild" type="date" name="start_date"></td>
+                </tr>
+                <tr>
+                    <th>End Date</th>
+                    <td><input class="input-feild" type="date" name="end_date"></td>
+                </tr>
+                <tr>
+                    <th>Add Image</th>
+                    <td><input class="input-file"  type="file" name="promo_image"></td>
+                </tr>
+                <tr>
+                    <td><input class="btn-res" type="submit" value="Add Promo" name="res-add-promo-submit"></td>
+                    <td><input class="btn-res" type="button" value="Back" onclick="window.location.href='promotions.php'"></td>
+                </tr>
+
+            </table>
+            <br><br>
+        </form>
+        </div>
+</div>
+    
+    </body>
+</html>

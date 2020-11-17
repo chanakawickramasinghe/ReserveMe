f4c8eb716bae28c83a24efb07195449ad2e50eba
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2020-11-17 16:53:52 +0530

    reset

--- reset/password_reset.php
+++ reset/password_reset.php
@@ -3,136 +3,20 @@
 <head>
 <meta charset="utf-8">
   <title>Reset</title>
-  <style>
-body{
-  margin: 0;
-  padding: 0;
-  font-family: sans-serif;
-  background: url(../images/bg.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  color: white;
-
-}
-
-.reset{
-    position: absolute;
-    top: 5%;
-    left: 50%;
-    transform: translate(-50%,50%);
-    width: 30%;
-    padding: 40px;
-    background: rgba(0,0,0,.8);
-    box-sizing: border-box;
-    box-shadow: 0 15px 25px rgba(0,0,0,.5);
-    border-radius: 10px;
-}
-
-
-.reset .inputBox
-{
-    position: relative;
-}
-
-.reset .inputBox input
-{
-    width: 100%;
-    padding: 14px 0;
-    font-size: 16px;
-    color: #fff;
-    letter-spacing: 1px;
-    margin-top: 2px;
-    margin-bottom: 15px;
-    border: none;
-    border-bottom: 1px solid #fff;
-    outline: none;
-    background: transparent;
-}
-
-.reset .inputBox label
-{
-   position: absolute;
-   top: 0;
-   left: 0;  
-   padding: 15px 0;
-   font-size: 18px; 
-   color: #fff;
-   pointer-events: none;
-   transition: .5s;
-
-}
-.reset .inputBox input:focus ~ label,
-.reset .inputBox input:valid ~ label 
-{
-    top: -18px;
-    left: 0;
-    color: #03a9f4;
-    font-size: 14px;
-
-} 
-
-.reset input[type="submit" i]
-{
-    margin-top: 8px;  
-    margin-left: 35%;
-    margin-bottom: -10px;
-    background: transparent;
-    border: none;
-    outline: none;
-    color: #fff;
-    background: #03a9f4;
-    padding: 10px 20px;
-    cursor: pointer;
-    border-radius: 5px;
-    display: inline-block;
-    font-size: 16px;
-}
-
-.reset input[type="submit" i]:hover
-{
-    background-color: #3e8e41;
-    color: white;
-}
-.anchor{
-  display: block;
-  position: relative;
-  top: 0px;
-  margin-bottom: 15px;
-    
-}
-.message{
-    color: white;
-}
-p{
-    color: white;
-}
-a {
-  position: relative;
-  top: -15px; 
-  left: -35px; 
-  text-decoration: none;
-  display: inline-block;
-  padding: 8px 16px;
-  
-}
-
-a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.previous {
-  font-size: 18px;
-  color: white;
-}
-
-
-.round {
-  border-radius: 50%;
-}
-
-
-</style>  
+  <link rel="stylesheet" href="../CSS/password_reset.css"/>
+  <link rel="icon" type="image/png" href="../images/logo.png" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Link the style sheets -->
+<link href="../CSS/login.css" rel="stylesheet" type="text/css" > 
+<!-- <link href="../CSS/main.css" rel="stylesheet" type="text/css"/> -->
+<link href="../CSS/nav.css" rel="stylesheet" type="text/css"/>
+<link href="../CSS/footer.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<!--FontAwesome-------->
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+<!--web-icon-->
+<!-- <link href="../images/logo.png" rel="shortcut icon"/> -->
 </head>
 <?php
     $alert='';
@@ -156,6 +40,7 @@ a:hover {
                     $alert="<div class='sent'>Password Reseted sucessfully.</div>";
                     $delete_query="DELETE FROM tokens WHERE token='$token'";
                     $delete_result=mysqli_query($connection,$delete_query);
+                    header( "Location:../includes/login.php");
                 }
                 else{
                     
@@ -173,6 +58,15 @@ a:hover {
     }
 ?>
 <body>
+    <!--Start of nav-->
+    <div class="topnav">
+        <a href="../index.php"><img class = "logo" src="../images/logo.png"></a>
+		<a class="navtab" href="../users/user-type.php">Register</a>
+		<a class="navtab" href="contact.php">Contact</a>
+		<a class="navtab" href="about.php">About</a>
+		<a class="navtab" href="javascript:void(0);" id="icon" onclick="onClickNav()"><i class="fa fa-bars"></i></a>    
+    </div>
+    <!--End of nav-->
     <div class="reset">
     <a href="../login.php" class="previous round">&#8249;</a>
         <?php echo $alert;?>

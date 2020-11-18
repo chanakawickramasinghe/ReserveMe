9cdaa2e26269bf14510462b5007eec4002928f4d
Aamir-97 <52451504+Aamir-97@users.noreply.github.com>
2020-11-18 22:20:12 +0530

    Content Change

--- users/employee/past-queries.php
+++ users/employee/past-queries.php
@@ -33,26 +33,23 @@
         </div>
 
         <table class="message-table">
-                <tr><th>I faced difficulties while login</th></tr>
-                <tr><td>When I try to login to the system, a message was shown as my account has been suspended and contact an admin to re-activate my account.</td></tr>
+
+                <tr><th>Shall you do for a special food order menu?</th></tr>
+                <tr><td>This is for childs party for nursary school. So we have a special food recipient to make. Can you make it?</td></tr>
                 <tr><td>from <b>John Henricks</b> on <b>2020-11-16 12:23:42</b></td></tr>
-                <tr><td class="sent-reply">You have violated the terms and conditions of the system so we are sorry to say that the account has been suspended by the administration. It is clearly specified in the Terms and Conditions that any violation can cause upto gettoing suspended your account.</td></tr>
+                <tr><td class="sent-reply">No Have no that spacial food menu for child function. our catering department is differnce.So you can contact on them to reqest your food menu.</td></tr>
                 <tr><td class="sent-reply">Reply sent on <b>2020-11-16 13:00:00</b> by <b>Co-admin Chanaka</b><hr>
-                <!-- <tr><td class="txt-area"><textarea placeholder="Enter Your Reply..." class="reply-area"></textarea></td></tr> -->
-                <!-- <tr><td class="txt-area"><button type="submit" class="btn-reply">Send</button></td></tr> -->
+               
 
                 <td><hr></td>
 
-                <tr><th>I faced difficulties while login</th></tr>
-                <tr><td>When I try to login to the system, a message was shown as my account has been suspended and contact an admin to re-activate my account.
-                When I try to login to the system, a message was shown as my account has been suspended and contact an admin to re-activate my account.</td></tr>
+                <tr><th>Are you having the event managment?</th></tr>
+                <tr><td>We are palnning for wedding reception.So could you arrage all the needs which we want to make it better.like Sound arrangements, Decoration and all. </td></tr>
                 <tr><td>from <b>Carl Johnson</b> on <b>2020-11-10 13:20:01</b></td></tr>
-                <tr><td class="sent-reply">You have violated the terms and conditions of the system so we are sorry to say that the account has been suspended by the administration. It is clearly specified in the Terms and Conditions that any violation can cause upto gettoing suspended your account.</td></tr>
+                <tr><td class="sent-reply">Yes we have event management in our restaurant.If you have any rrequest please contact through this for more information. Contact No:+112536486.</td></tr>
                 <tr><td class="sent-reply">Reply sent on <b>2020-11-16 13:00:00</b> by <b>Co-admin Chanaka</b><hr>
-                <!-- <tr><td class="txt-area"><textarea placeholder="Enter Your Reply..." class="reply-area"></textarea></td></tr> -->
-                <!-- <tr><td class="txt-area"><button type="submit" class="btn-reply">Send</button></td></tr> -->
-
-            </table>
+                
+        </table>
 
     </body>
 </html>

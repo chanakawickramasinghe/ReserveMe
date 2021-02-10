4fea7afcd1fce8f8ce998614dac2ee530fd0503a
Nuwan Fernando <54792829+lifeofhassa@users.noreply.github.com>
2021-02-10 15:50:21 +0530

    Update add-menu.php

--- users/restaurant/add-menu.php
+++ users/restaurant/add-menu.php
@@ -1,32 +1,56 @@
 <?php include 'add-menu-submit.php'; ?>
+<?php include('../../includes/connection.php') ?>
+<?php include('../../includes/session.php') ?>
+
 
 <!DOCTYPE html>
-<html lang = "en" dir = "ltr">
+<html lang="en">
+
 <head>
-<meta charset="UTF-8">
+
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Menu</title>
-    <link rel="stylesheet" href="../../CSS/main.css">
-    <script src="https://kit.fontawesome.com/20026fc328.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="../../CSS/nav.css">
-    <link rel="stylesheet" href="../../CSS/search.css">
+    <title>Add Menue</title>  
+    <link href="../../images/logo.png" rel="shortcut icon"/> 
+    <!-- Link the style sheets -->
     <link rel="stylesheet" href="../../CSS/pre-order.css">
-    <!--web-icon-->
-    <link href="../../images/logo.png" rel="shortcut icon"/>
+    <link rel="stylesheet" href="../../CSS/main.css">
+    <link rel="stylesheet" href="../../CSS/nav.css">   
+    <link rel="stylesheet" href="../../CSS/footer.css">
+
 </head>
 
 <body>
+    <?php include('navigations.php'); ?>
+   
+    <!-- Start of side bar -->
+    <div class="sidebar"> 
+      <a href="restaurant-home.php">Home</a> 
+      <a href="add-preordermenu.php">Pre - Order Menu</a>    
+      <a href="add-menu.php" class="active">Update the Menu</a>
+      <a href="res-promo.php">Promotions</a>
+      <a href="res-floor-plan.php">Floor Plan</a> 
+      <a href="res-reservation.php">Reservations</a>
+      <a href="../customer/res_view/review/index.html?res_id=4">View Reviews</a>   
+      <a href="res-contactus.php">Help Desk</a>
+      <br>
+      <hr>
+      <a href="emp-iframe.php">Manage Employee</a>
+      <a href="hall-details.php">Reception Hall</a>
+    </div>
+    <!-- End of side bar -->
+
+    <div class="content">
+    <div style="margin:20px">     
 
   <!--Start of getting photos-->
     <div id="content">
         <form action="add-menu-submit.php" method="post" enctype="multipart/form-data">
             <h2 class="error-msg" style="font-size:14px"><?php include_once('../../includes/message.php'); ?></h2>
-                <br><br>
-                <center>
-                <h1 style="color:#ffbb01;"><font color="black">Add</font> Menu</h1>
+
+                <center><h2 style="color:#ffbb01;"><font color="black">Add</font> Menu</h2>
                 <br>
                 <p><h3>Insert images of your Restaurant Menu seperately.</h3></p>
-                <p>(Only Five images are allowed)</p>
                 <br>
                 <!-- <h3>Rename Image</h3>
                 <br>

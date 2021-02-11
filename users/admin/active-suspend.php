2f1f62bfb1123ced71a831767dd6ce685ba60ac2
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2021-02-11 23:58:34 +0530

    manage_users

---
+++ users/admin/active-suspend.php
@@ -0,0 +1,28 @@
+<?php
+
+include('../../includes/connection.php');
+
+$id = $_POST['id'];
+echo "$id";
+
+if(isset($_POST['btn-activate-cus-account'])){
+
+
+    $update_sql = "UPDATE customer
+    SET active_status = 1 WHERE user_id = '$id' ";
+    
+    if (mysqli_query($connection,$update_sql) == TRUE) {
+        $message = base64_encode(urlencode("Updated Successful"));
+        header('Location:./coadmins.php?msg=' . $message);
+        exit();
+    } 
+    
+    else {
+         $message = base64_encode(urlencode("SQL Error"));
+         header('Location:./coadmins.php?msg=' . $message);
+         exit();
+    }
+}
+
+
+?>

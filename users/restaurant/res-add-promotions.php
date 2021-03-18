707f62aada78da17a03ae504145f4cf766fce214
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2021-03-18 20:33:38 +0530

    promotion done

--- users/restaurant/res-add-promotions.php
+++ users/restaurant/res-add-promotions.php
@@ -37,14 +37,15 @@
 
     <br>
     <div class="content">
+    <center><h1 style="color:#ffbb01;"><font color="black">Add</font> Promotions</h1><center>
+    <br/>
             <div class="promo-btn-bar">
-                <button type="button" class="btn-promo active" onclick="window.location.href='#'">Add Promotion</button>
-                <button type="button" class="btn-promo" onclick="window.location.href='res-promo.php'">Ongoing Promotion</button>
-                <button type="button" class="btn-promo" onclick="window.location.href='res-past-promo.php'">View Past Promotions</button>
+                <button type="button" class="btn-promos" onclick="window.location.href='res-promo.php'">Ongoing Promotion</button>
+                <button type="button" class="btn-promos active" onclick="window.location.href='#'">Add Promotion</button>
+                <button type="button" class="btn-promos" onclick="window.location.href='res-past-promo.php'">View Past Promotions</button>
             </div>
 
-            <center><h1 style="color:#ffbb01;"><font color="black">Add</font> Promotions</h1><center>
-
+    <br/>       
 	<br>
     	<div class="login-box"> 
         
@@ -53,24 +54,24 @@
         <form class="add-promo-form" method="post" action="res-promo.php" enctype="multipart/form-data"> <!--SHOULD BE action="res-add-promotion-submit.php"-->
             <table class="add-promo-form">
                 <tr>
-                    <th>Starting Date</th>
-                    <td><input class="input-feild" type="date" name="start_date"></td>
+                    <th>Starting Date & Time</th>
+                    <td><input class="input-feild" type="datetime-local" name="start_date" required></td>
                 </tr>
                 <tr>
-                    <th>End Date</th>
-                    <td><input class="input-feild" type="date" name="end_date"></td>
+                    <th>End Date & Time</th>
+                    <td><input class="input-feild" type="datetime-local" name="end_date" required></td>
                 </tr>
                 <tr>
-                    <th>Category (Paid*/Unpaid*)</th>
+                    <th>Description (If any)</th>
                     <td><input class="input-feild"  type="text" name="Category"></td>
                 </tr>
                 <tr>
                     <th>Add Image</th>
-                    <td><input class="input-file"  type="file" name="promo_image"></td>
+                    <td><input class="input-file"  type="file" name="promo_image" required></td>
                 </tr>
                 <tr>
                     <td><input class="btn-res" type="submit" value="Add Promo" name="res-add-promo-submit"></td>
-                    <td><input class="btn-res" type="button" value="Back" onclick="window.location.href='res-promo.php'"></td>
+                    <td><input class="btn-res" type="reset" value="Clear"></td>
                 </tr>
 
             </table>
--- users/restaurant/res-promo.php
+++ users/restaurant/res-promo.php
@@ -1,11 +1,12 @@
 <?php include('../../includes/session.php') ?>
+<?php include('../../includes/connection.php') ?>
 
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Restaurant-Promotions</title>
-        <link rel="stylesheet" href="../../CSS/admin-nav.css">
+        <link rel="stylesheet" href="../../CSS/nav.css">
         <link href="../../CSS/footer.css" rel="stylesheet" type="text/css">  
         <link href="../../images/logo.png" rel="shortcut icon"/>
         <link rel="stylesheet" href="../../CSS/res-promo.css"/>
@@ -36,38 +37,58 @@
 
         <div class="content">
             
+        <center><h1 style="color:#ffbb01;"><font color="black">Ongoing</font> Promotions</h1></center>
+        <br/>  
+        <div class="promo-btn-bar">
+            <button type="button" class="btn-promos active" onclick="window.location.href='#'">Ongoing Promotion</button>
+            <button type="button" class="btn-promos" onclick="window.location.href='res-add-promotions.php'">Add Promotion</button>
+            <button type="button" class="btn-promos" onclick="window.location.href='res-past-promo.php'">View Past Promotions</button>
+        </div>
+        <br/>
+            
+        <?php
 
-            <div class="promo-btn-bar">
-                <button type="button" class="btn-promo" onclick="window.location.href='res-add-promotions.php'">Add Promotion</button>
-                <button type="button" class="btn-promo active" onclick="window.location.href='#'">Ongoing Promotion</button>
-                <button type="button" class="btn-promo" onclick="window.location.href='res-past-promo.php'">View Past Promotions</button>
-            </div>
+            //get date to check
+            date_default_timezone_set('Asia/Colombo');
+            $today_date = date("Y-m-d H:i:s");
 
-            <center><h1 style="color:#ffbb01;"><font color="black">Ongoing</font> Promotions</h1></center>
+            $promos = "SELECT * FROM promotions WHERE end_date>'$today_date'"; //check with date
 
-            <table class="promo" id="myTable" border="1">
+            $promo_query = mysqli_query($connection,$promos);
+            $num_promos = mysqli_num_rows($promo_query);
+
+            if ($num_promos == 0){ //if there are no current promotions
+                echo"<p>No Past Promotions to Display</p>";
+            }
+
+            else { //to see current promos
+            echo "
+            <table class=\"promo\" id=\"myTable\" border=\"1\">
                 <tr>
+                    <th>Promo ID</th>
                     <th>Promo Image</th>
-                    <th>Posted Date & Time</th>
+                    <th>Post Date & Time</th>
                     <th>End Date & Time</th>
-                    <th>Category</th>
-                    <th>Action</th>
-                </tr>
-                <tr>
-                    <td><img class="img-promo" src="../../images/promos/1.jpg"></td>
-                    <td>2020-10-15 22:03:02</td>
-                    <td>2020-10-22 22:03:02</td>
-                    <td>Paid</td>
-                    <td><button type="button" class="btn-promo-remove">Remove</button></td>                
-                </tr>
-                <tr>
-                    <td><img class="img-promo" src="../../images/promos/4.jpg"></td>
-                    <td>2020-10-20 09:00:00</td>
-                    <td>2020-10-22 09:00:00</td>
-                    <td>Unpaid</td>
-                    <td><button type="button" class="btn-promo-remove">Remove</button></td>                
-                </tr>  
-            </table>
+                    <th>Description</th>
+                <th>Action</th>
+            </tr> ";
+
+            while($row = mysqli_fetch_assoc($promo_query)){
+            echo"
+            <tr> 
+                <td>".$row['promo_id']."</td> 
+                <td><img class=\"img-promo\" src= \"../../images/promos/{$row['image']}\" alt=\"image not found\"></td>                     
+                <td>".$row['start_date']."</td>
+                <td>".$row['end_date']."</td>
+                <td>".$row['text']."</td>
+                <td><a class=\"btn-promo-remove\" href =res-remove-promo.php?id='".$row['promo_id']."' > Remove </a> </td>
+
+            </tr>";
+            }
+            echo"
+            </table> ";
+        }
+        ?>
         </div>
 
     <!--script for onClickNav() for the navigation menu-->

3cbbf435eef4e7c1bb85ef351600d804a2326a8c
Aamir-97 <52451504+Aamir-97@users.noreply.github.com>
2021-03-21 12:28:43 +0530

    Employee session added

--- includes/session.php
+++ includes/session.php
@@ -7,16 +7,3 @@ function checkSession(){
 }
 ?>
 
-<?php 
-	// session_start();
-	// if(isset($_POST['logout'])) {
-	// 	$_SESSION['login'] = false;
-    //     unset($_SESSION['login']);
-    //     unset($_SESSION['orgID']);
-	// }
-	// if(isset($_SESSION['login']) && $_SESSION['login'] == true && $_SESSION['userType'] == 5) {
-	
-	// } else {
-	// 	header("Location:../../pages/advertiser/AdvertLogin.php");
-	// }
-?>

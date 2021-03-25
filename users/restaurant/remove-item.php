73207513cf770206e1564e64ca29f5784242955e
Aamir-97 <52451504+Aamir-97@users.noreply.github.com>
2021-03-25 16:06:49 +0530

    Edited update the menu page

--- users/restaurant/remove-item.php
+++ users/restaurant/remove-item.php
@@ -1,10 +1,13 @@
 <?php include('../../includes/connection.php') ?>
+<?php include('../../includes/session.php') ?>
+
 
 <?php
 
 if(isset($_GET['id'])){
+    // echo $_GET['id']; 
     
-$remove_sql = "DELETE FROM menu WHERE promo_id = ".$_GET['id'];
+$remove_sql = "DELETE FROM menu WHERE item_id = ".$_GET['id'];
 mysqli_query($connection, $remove_sql);
 
 $result = mysqli_query($connection,$remove_sql);
@@ -13,7 +16,8 @@ if($result){
     header("Location:add-menu.php");
 }
 else{
-    header("Location:add-menu.php");
+    echo "SQL syntex Error";
     }
 }
+
 ?>

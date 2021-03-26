d44ba980b929680883894f49de2561c97afc7db2
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2021-03-27 02:53:42 +0530

    mng emp half done

--- users/restaurant/add-hall.php
+++ users/restaurant/add-hall.php
@@ -1,17 +1,19 @@
+<?php include('../../includes/session.php') ?>
+<?php include('../../includes/connection.php') ?>
+
 <html>  
 <head>
-    <title>Hall Registration</title> 
-    <link href="../../images/logo.png" rel="shortcut icon">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Link the style sheets -->
-    <link href="../../CSS/login.css" rel="stylesheet" type="text/css" > 
-    <link href="../../CSS/nav.css" rel="stylesheet" type="text/css">
-    <link href="../../CSS/footer.css" rel="stylesheet" type="text/css">
-    <!-- <link href="../../CSS/pre-order.css" rel="stylesheet" type="text/css">     
-    <link href="../../CSS/res-promo.css" rel="stylesheet" type="text/css" >  -->
-    <link href="../../CSS/add-hall.css" rel="stylesheet" type="text/css" >
-    <script crossorigin="anonymous" src="https://kit.fontawesome.com/70a642cd7c.js"></script>
 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Hall</title> 
+    <link href="../../images/logo.png" rel="shortcut icon"/> 
+    <!-- Link the style sheets -->
+    <link rel="stylesheet" href="../../CSS/pre-order.css">
+    <link rel="stylesheet" href="../../CSS/main.css">
+    <link rel="stylesheet" href="../../CSS/nav.css">   
+    <link rel="stylesheet" href="../../CSS/footer.css">
+    <link rel="stylesheet" href="../../CSS/hall.css">
     <style>
         .signin {
             padding:10px;
@@ -21,118 +23,82 @@
         }
     </style> 
 </head>
-    <body>
-    <!-- nav bar -->
-    <?php include('navigation.php'); ?> 
-    
-    <center>
-        
-        <h1 style="color:#ffbb01;"><font color="black">Add Hall</font> Details</h1>
+<body>
+
+      <!-- nav bar -->
+      <?php include('navigation.php'); ?>
+
+<!-- Start of side bar -->
+ <div class="sidebar"> 
+ <a href="restaurant-home.php">Home</a>     
+ <a href="add-menu.php">Update the Menu</a>
+ <a href="res-promo.php" >Promotions</a>
+ <a href="res-floor-plan.php" >Floor Plan</a> 
+ <a href="res-reservation.php">Reservations</a>
+ <a href="hall-reservation.php">Hall Reservations</a>
+ <a href="res-review.html">View Reviews</a>   
+ <br>
+ <hr>
+ <a href="mng-emp.php">Manage Employee</a>
+ <a href="hall-details.php" class="active">Reception Hall</a>
+</div>
+<!-- End of side bar -->
+
+   
+   <div class="content">
+    <div style="margin:20px">  
+
+
+        <h1 style="color:#ffbb01; text-align:center;"><font color="black">Add Hall</font> Details</h1>
+        <div class="add-hall-box">
         <form method="post" action="add-hall-submit.php" enctype="multipart/form-data">
-        <br><br>
-        <table>
+        
+        <table class="add-hall">
             <tr>
                 <th>Hall Name</th>
-                <td><input type="text" name="hall_name" placeholder="Hall Name"></td>
+                <td><input class="type-feild-l" type="text" name="hall_name" placeholder="Hall Name" required></td>
             </tr>
             <tr>
                 <th>Contact No.</th>
-                <td><input type="text" name="contact_no" placeholder="Contatc No." pattern='^\+?\d{0,13}'></td>
+                <td><input class="type-feild-l" type="text" name="contact_no" placeholder="Contatc No." pattern='^\+?\d{0,13}' required></td>
             </tr>
             <tr>
                 <th>Profile Image</th>
-                <td><input type="file" name="main_image" placeholder="Select Profile Image"></td>
+                <td><input class="type-feild-l" type="file" name="main_image" placeholder="Select Profile Image" required></td>
             </tr>
             <tr>
                 <th>Image 1</th>
-                <td><input type="file" name="image1" placeholder="Image 1"></td>
+                <td><input class="type-feild-l" type="file" name="image1" placeholder="Image 1"></td>
             </tr>
             <tr>
                 <th>Image 2</th>
-                <td><input type="file" name="image2" placeholder="Image 2"></td>
+                <td><input class="type-feild-l" type="file" name="image2" placeholder="Image 2"></td>
             </tr>
             <tr>
                 <th>Address</th>
-                <td><input type="text" name="address" placeholder="Address"></td>
+                <td><input class="type-feild-l" type="text" name="address" placeholder="Address" required></td>
             </tr>
             <tr>
                 <th>Capacity</th>
-                <td><input type="text" name="capacity" placeholder="Capacity"></td>
+                <td><input class="type-feild-l" type="text" name="capacity" placeholder="Capacity" required></td>
             </tr>
             <tr>
                 <th>Description</th>
-                <td><textarea rows="5" cols="40" name="description" placeholder="Description (below 250 words)"></textarea></td>
+                <td><textarea class="type-feild-l" rows="5" cols="40" name="description" placeholder="Description (below 250 words)" required></textarea></td>
             </tr>
             <tr>
                 <th>Advance Fee</th>
-                <td><input type="text" name="advance" placeholder="Rs."></td>
+                <td><input class="type-feild-m" type="text" name="advance" placeholder="Rs." required></td>
             </tr>
-            <center>
-        </table>
-        <br><br><br><br>
+        </table> 
+    </center>
+        <br/>
+            <input type="submit" name="btn-add-hall" style="margin-left:250px; margin-top:-30px" class="btn-add-hall-l" value="Add Hall Details" required></th>
+            <button type="button" class="btn-add-hall-s" onclick="window.location.href='hall-details.php'">Back</button></td>
 
-        <input type="submit" name="btn-add-hall" style="margin-left:250px; margin-top:-30px" class="btn-promo" value="Add Hall Details" required>
-
-        
-        <br><br><br><br>
-        <!-- <input type="rest" name="submit" style="margin-left:250px; margin-top:-30px" class="btn-promo" value="Clear" required>
-        </form> -->
-
+        </form>
+        </div>
         
     </body>
     
-<!-- <body class="login-body">
-
-  
-        <div class="login-box"> 
-               
-            <img src="../../images/form_icons/regform.png" class="avatar">
-            <br>
-                <h1 align="center">Add Reception Halls</h1><br>
-
-            <h2 class="error-msg"><?php include_once('../../includes/message.php'); ?></h2>
-
-                <form action="hall-add-submit.php" method="post">                   
-                  
-                <table style="width:750px;float:center;align-items:center">
-                <tr>
-                    <td>
-                    <img src="../../images/form_icons/user.png" alt="user" class="icons">
-                    <label for="name">Hall Name</label><br>                   
-                    <input type="text"  name="name" class="reg-type-feild" placeholder="Employee Name" required>    
-
-                    <p>
-                    <img src="../../images/form_icons/phone-call.png" alt="user" class="icons">
-                    <label for="contact">Contact number</label><br>
-                    <input type="tel" name="contact" class="reg-type-feild" placeholder="Employee Mobile Number" pattern='^\+?\d{0,13}' >
-                    </p>
-
-                    <p>
-                    <img src="../../images/form_icons/map.png" alt="user" class="icons">
-                    <label for="location">Location URL</label><br>
-                    <input type="url" name="url" id='url' class="reg-type-feild"  placeholder="Enter Restaurant Location" required>
-                    </p>
-
-                    <p>
-                    <img src="../../images/form_icons/map.png" alt="user" class="icons">
-                    <label for="url">Capacity</label><br>
-                    <input type="url" name="url" id='url' class="reg-type-feild"  placeholder="Enter Restaurant Location" required>
-                    </p>
-
-                    <p>
-                    <img src="../../images/form_icons/image.png" alt="user" class="icons">
-                    <label for="image"> Profile Image</label><br>
-                    <input type="file" name="res_img" class="reg-type-feild" placeholder="Upload Profile Image">
-                    </p> 
-
-                    <p align="center"> <input type="submit" name="submit"  class="btn" value="Submit" required></p>
-                   <br>
-                    </td>
-                </tr> 
-                </table>          
-            </form>    
-            
-        </div>
-
-</body> -->
 </html>

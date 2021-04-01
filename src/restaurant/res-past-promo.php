63f873b1ea166b37f0894b4e083c53939259fee3
Nuwan Fernando <54792829+lifeofhassa@users.noreply.github.com>
2021-04-01 09:48:23 +0530

    Update res-past-promo.php

--- src/restaurant/res-past-promo.php
+++ src/restaurant/res-past-promo.php
@@ -26,7 +26,6 @@ include('../../public/includes/message.php');?>
       <a href="restaurant-home.php">Home</a>     
       <a href="add-menu.php">Update Menu</a>
       <a href="res-promo.php"  class="active">Promotions</a>
-      <a href="res-floor-plan.php">Floor Plan</a> 
       <a href="res-reservation.php">Table Reservations</a>
       <a href="hall-reservation.php">Hall Reservations</a>
       <a href="res-review.php">View Reviews</a>     
